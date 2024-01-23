ae2e7993f16c57f6f5c05986a9267e146c99d241
h602408 <602408@stud.hvl.no>
2024-01-23 11:28:38 +0100

    lagt til noen klasser, og gjordt det basic på brikke

--- Brikke.java
+++ Brikke.java
@@ -1,2 +1,10 @@
 public class Brikke {
+
+    private final String farge;
+    private Rute posisjon;
+
+    public Brikke(String farge, Rute posisjon){
+        this.farge = farge;
+        this.posisjon = posisjon;
+    }
 }
