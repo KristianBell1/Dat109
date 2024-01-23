a0f8456c47505e4545daa2b75d47dd76366183c1
h602408 <602408@stud.hvl.no>
2024-01-23 12:02:42 +0100

    gjordt spiller

--- Spiller.java
+++ Spiller.java
@@ -1,2 +1,17 @@
 public class Spiller {
+
+    private final String navn;
+    private final Brikke brikke;
+
+    public Spiller(String spillerNavn, Brikke brikke) {
+        this.navn = spillerNavn;
+        this.brikke = brikke;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+    public Brikke getBrikke() {
+        return brikke;
+    }
 }
